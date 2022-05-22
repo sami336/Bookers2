638c1f3d72f49cbd4a23fb92ff72b94efe2995a7
SamiIzawa <93434132+sami336@users.noreply.github.com>
2022-05-22 05:20:26 +0000

    [Update]aboutページのレイアウト作成まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
   root to: "homes#top"
+  get 'home/about', to: 'homes#about', as: 'about'
   devise_for :users
   resources :users, only: [:show, :edit, :index]
   resources :books, only: [:show, :edit, :index, :destroy]
