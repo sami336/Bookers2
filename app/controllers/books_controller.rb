012c166e4153ad0f87d7b26f55cabc6699c74a37
SamiIzawa <93434132+sami336@users.noreply.github.com>
2022-05-25 14:40:37 +0000

    [Update]エラー残り８つ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,14 +13,20 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id), notice: "You have created book successfully."
+    if @book.save
+      redirect_to book_path(@book.id), notice: "You have created book successfully."
+    else
+      @user = current_user
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
-    @user = current_user
-    @new_book = Book.new
     @book = Book.find(params[:id])
+    @user = @book.user
+    @new_book = Book.new
+
   end
 
   def destroy
@@ -31,12 +37,22 @@ class BooksController < ApplicationController
 
   def edit
    @book = Book.find(params[:id])
+    if @book.user == current_user
+      render :edit
+    else
+      @user = current_user
+      @books = Book.all
+      render :index
+    end
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id), notice: "You have updated book successfully."
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: "You have updated book successfully."
+    else
+      render :edit
+    end
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
+  validates :name, uniqueness: true, length: { in: 2..20 } 
+  validates :introduction, length: { maximum: 50 }
 
   def get_profile_image(width, height)
     unless profile_image.attached?
