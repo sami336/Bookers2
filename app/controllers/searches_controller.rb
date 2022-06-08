af67f65fca468d7cfe69ee9ed9682ea654943229
SamiIzawa <93434132+sami336@users.noreply.github.com>
2022-06-08 05:04:44 +0000

    [Update]検索機能

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,11 +1,13 @@
 class SearchesController < ApplicationController
  before_action :authenticate_user!
- 
+
   def search
     @range = params[:range]
+    @word = params[:word]
+    @search = params[:serch]
     @book = Book.all
-    
-    if @range == "User"
+
+    if @range == "user"
       @users = User.looks(params[:search], params[:word])
     else
       @books = Book.looks(params[:search], params[:word])
